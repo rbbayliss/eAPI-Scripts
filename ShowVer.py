bd95fa487ed752934e81366378d71f79e1e32fb5
Rich Bayliss <richard@thescarydoor.com>
2017-05-11 11:59:23 -0700

    Update ShowVer.py
    Correcting my stupid typo

--- ShowVer.py
+++ ShowVer.py
@@ -66,7 +66,7 @@ def main(screen):
    win.addstr(22,0,"          [ . ] = Switch probably rebooting")
    refresh() 
 
-   for x in range(numStudents):
+   for x in range(NumStudents):
       t = Thread(target=GetVersion, args=(x+1,))
       t.start()
 
